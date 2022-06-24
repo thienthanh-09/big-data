cdcf3a14e2379bf916cf955240198f1d92a07ad1
thienthanh-09 <19520952@gm.uit.edu.vn>
2022-06-24 08:28:07 +0700

    hic

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.views.i18n import JavaScriptCatalog
-from store.views import account, product, store, order, comment, payment, notification, chat, utils
+from store.views import account, product, store, order, comment, utils
 from .views.dashboard import dashboard
 from .views.render import product as render_product, dashboard as render_dashboard
 from django.views.decorators.cache import cache_page
@@ -16,25 +16,15 @@ urlpatterns = [
     path('reset/done/', account.ResetPasswordDoneView.as_view(), name='password_reset_done'),
     path('reset/confirm/<uidb64>/<token>/', account.ConfirmPasswordResetView.as_view(), name="password_reset_confirm"),
 
-    path('chat/', chat.ChatView.as_view()),
-    path('chat/start/', chat.StartChat.as_view()),
-    path('chat/message/', chat.ChatMessage.as_view()),
-
     path('products/', product.ProductList.as_view(), name='products'),
     path('product/<pk>/<slug>/', product.ProductDetail.as_view(), name='product_detail'),
     path('product/', product.ProductView.as_view()),
     path('comment/<pk>/', comment.CommentView.as_view()),
-    path('history/', order.OrderHistoryView.as_view(), name='order_history'),
 
     path('account/', account.AccountView.as_view(), name='account'),
     path('account/profile/<pk>/', account.UpdateProfileView.as_view(), name='profile'),
     path('account/change-password/', account.ChangePasswordView.as_view(), name='change_password'),
     path('store/<slug>/', store.StoreView.as_view(), name='store'),
-
-    # path('product/<pk>/like/', product.LikeProductView.as_view(), name='like'),
-    path('add-wishlist/', product.LikeProductView.add, name='like'),
-    path('remove-wishlist/<int:wishlist_id>/', product.LikeProductView.delete, name="remove-wishlist"),
-    path('favorite/', product.FavoriteProduct.as_view(), name='favorite_product'),
 
     path('me/store/', store.MyStoreView.as_view()),
     path('me/store/statistics/', store.StoreStatisticsView.as_view(), name='dashboard_store'),
@@ -46,17 +36,6 @@ urlpatterns = [
     path('me/store/product/remove/<pk>/', store.RemoveProduct.as_view()),
     path('me/store/product/sale/<pk>/', store.ProductSaleView.as_view()),
 
-    path('me/cart/add/', order.AddCartView.as_view()),
-    path('me/cart/remove/<pk>/', order.RemoveCartView.as_view()),
-    path('me/cart/', order.CartView.as_view(), name='cart'),
-    path('me/cart/purchase/', order.PurchaseView.as_view()),
-    path('me/cart/purchase/payment/<pk>/', payment.PaymentMethodView.as_view()),
-    path('me/cart/purchase/thank-you/', order.FinishPurchaseView.as_view()),
-
-    path('me/notification/', notification.NotificationListView.as_view(), name='notification'),
-    path('me/notification/get/', notification.GetNotificationView.as_view()),
-    path('me/notification/read/<pk>/', notification.ReadNotificationView.as_view()),
-    path('me/notification/read-all/', notification.ReadAllNotificationView.as_view()),
     path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
 
     path('currency/', utils.get_currency, name='currency'),
